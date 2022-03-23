5f8ae362271587bccb81202f9f2f84035ee193a7
qianxi0410 <894871277@qq.com>
2022-03-23 09:38:42 +0800

    chore: move to APP_API_URL

--- src/api/axios/axios.ts
+++ src/api/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.BASE_URL || "http://localhost:8080",
+  baseURL: (import.meta.env.APP_API_URL as string) || "http://localhost:8080",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
