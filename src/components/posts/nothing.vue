4739ef37d8a066a1bbb15e1f2313ed093eb3faef
qianxi0410 <894871277@qq.com>
2022-03-24 09:29:08 +0800

    chore: fix typo

--- src/components/posts/nothing.vue
+++ src/components/posts/nothing.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="row justify-center">
-    <div class="col-md-6 col-xs-10 col-sm-9 text-h4 text-grey">Nothig post :p</div>
+    <div class="col-md-6 col-xs-10 col-sm-9 text-h4 text-grey">Nothing post :p</div>
   </div>
 </template>
