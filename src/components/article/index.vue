40a0cd116a3ef3b87cc2f6bca8868fbed6b4f68e
qianxi0410 <894871277@qq.com>
2022-03-26 19:02:41 +0800

    feat: finish basic

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { evaArrowLeftOutline, evaArrowRightOutline } from "@quasar/extras/eva-icons";
+import {
+  evaArrowheadUpOutline,
+  evaArrowLeftOutline,
+  evaArrowRightOutline,
+} from "@quasar/extras/eva-icons";
 import { getPost } from "../../api/post";
 import { Post } from "../../types";
 
@@ -15,46 +19,90 @@ const create_date_fmt = computed(() => {
 });
 
 const route = useRoute();
+const router = useRouter();
+
+const to = (id: string) => {
+  router.push({ path: `/posts/${id}` });
+};
+
+const scroll = () => {
+  const el = document.getElementById("app");
+  if (el) el.scrollIntoView({ behavior: "smooth" });
+};
+
+const { y } = useWindowScroll();
+const { height } = useWindowSize();
 
 onMounted(async () => {
   const id = route.params.id;
+
   if (id) {
     const { data } = await getPost(id as string);
     Object.assign(post, data.data);
+    window.history.pushState(null, "", `/posts/${post.display_id}`);
+
+    if (!post.display_id) {
+      router.push({ path: `/404` });
+    }
   }
 });
 </script>
 
 <template>
-  <div class="row justify-center">
-    <div class="col-md-6 col-xs-10 col-sm-9 text-h3">{{ post.title }}</div>
-  </div>
-  <div class="row justify-center">
-    <div class="col-md-6 col-xs-10 col-sm-9 text-body1 text-grey q-mt-md">
-      {{ create_date_fmt }} &nbsp;·&nbsp;
-      <span>{{ post.author }}</span>
+  <div v-if="post.id">
+    <Transition mode="out-in" name="fade">
+      <q-btn
+        v-if]="y > height * 1.1"
+        round
+        flat
+        :ripple="false"
+        :icon="evaArrowheadUpOutline"
+        class="fixed-bottom-right"
+        @click="scroll"
+      />
+    </Transition>
+    <div class="row justify-center">
+      <div class="col-md-6 col-xs-10 col-sm-9 text-h3">{{ post.title }}</div>
+    </div>
+    <div class="row justify-center">
+      <div class="col-md-6 col-xs-10 col-sm-9 text-body1 text-grey q-mt-md">
+        {{ create_date_fmt }} &nbsp;·&nbsp;
+        <span>{{ post.author }}</span>
+      </div>
+    </div>
+    <Markdown :text="post.content!" />
+    <div class="row justify-center">
+      <q-toolbar class="col-md-5 col-sm-8 col-xs-9 text-center text-grey">
+        <q-btn
+          v-show="post.prev"
+          :icon="evaArrowLeftOutline"
+          rounded
+          flat
+          :ripple="false"
+          @click="to(post.prev)"
+        ></q-btn>
+        <q-space />
+        <q-btn
+          v-show="post.next"
+          :icon="evaArrowRightOutline"
+          rounded
+          flat
+          :ripple="false"
+          @click="to(post.next)"
+        ></q-btn>
+      </q-toolbar>
     </div>
   </div>
-  <Markdown v-if="post.id" :text="post.content!" />
-  <div class="row justify-center">
-    <q-toolbar class="col-md-5 col-sm-8 col-xs-9 text-center text-grey">
-      <q-btn
-        v-show="post.prev"
-        :icon="evaArrowLeftOutline"
-        rounded
-        flat
-        :ripple="false"
-        :to="`${post.prev}`"
-      ></q-btn>
-      <q-space />
-      <q-btn
-        v-show="post.next"
-        :icon="evaArrowRightOutline"
-        rounded
-        flat
-        :ripple="false"
-        :to="`${post.next}`"
-      ></q-btn>
-    </q-toolbar>
-  </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
