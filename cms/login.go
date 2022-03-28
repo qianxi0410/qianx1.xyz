b72d6befcc5d8133bc164030f04113a66d0196cd
qianxi0410 <894871277@qq.com>
2022-03-28 22:11:58 +0800

    feat: login to server

--- cms/login.go
+++ cms/login.go
@@ -1,6 +1,37 @@
 package main
 
-func login() (string, error) {
+import (
+	"encoding/json"
+	"net/http"
+	"os"
 
-	return "", nil
+	"github.com/go-resty/resty/v2"
+)
+
+// login is a pre function that is called before the execution func
+// we need to get jwt token from the server
+// and set it to the request header
+func login() (string, error) {
+	cli := resty.New()
+
+	resp, err := cli.
+		R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(User{
+			Username: os.Getenv("USERNAME"),
+			Password: os.Getenv("PASSWORD"),
+		}).
+		Post("https://blogapi.qianx1.xyz/user/login")
+
+	if err != nil || resp.StatusCode() != http.StatusOK {
+		return "", err
+	}
+
+	var r R
+	err = json.Unmarshal(resp.Body(), &r)
+	if err != nil {
+		return "", err
+	}
+
+	return r.Data, nil
 }
